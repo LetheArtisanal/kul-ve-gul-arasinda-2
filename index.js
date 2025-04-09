dfb2ce361518d573e14d8b4f15258d49d3939df9
LetheArtisanal <letheartisanal@gmail.com>
2025-04-09 18:40:43 +0300

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ fontFamily: 'sans-serif', textAlign: 'center', padding: '50px' }}>
-      <h1>Kül ve Gül Arasında</h1>
-      <p>Şiirsel seramiğin dijital vitrinine hoş geldiniz.</p>
-    </div>
-  );
-}
